8a99a6846803fe157c8f956a0e981f5701eb008e
Ali Rıza Reisoğlu <alirizareisoglu@gmail.com>
2022-03-18 16:01:38 +0300

    Initial Commit

---
+++ SwiiftUITutorials/SwiiftUITutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiiftUITutorialsApp.swift
+//  SwiiftUITutorials
+//
+//  Created by Ali Riza Reisoglu on 18.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiiftUITutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
