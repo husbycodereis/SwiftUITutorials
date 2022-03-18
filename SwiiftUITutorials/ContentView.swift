220b2def445295b21a4f4d709b91021ff1e0b2cb
Ali Rıza Reisoğlu <alirizareisoglu@gmail.com>
2022-03-18 16:42:57 +0300

    swift ui initial commit

--- SwiiftUITutorials/ContentView.swift
+++ SwiiftUITutorials/ContentView.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
     var body: some View {
+        SampleVerticalView()
+    }
+}
+
+fileprivate func SampleVerticalView() -> some View {
+    return VStack{
+        Spacer(minLength: 300)
         Text("Hello, world!")
+            .padding()
+        Text("Hello, cano!")
+            .padding()
+        SampleHorizontalView()
+        SampleStackView()
+        Spacer(minLength: 300)
+       
+    }
+}
+
+fileprivate func SampleHorizontalView() -> some View {
+    return HStack{
+        Text("Hello, world!")
+            .padding()
+        Text("Hello, cano!")
+            .padding()
+    }
+}
+
+fileprivate func SampleStackView() -> some View {
+    return ZStack(alignment: Alignment.bottom){
+        Color.purple
+        Text("Hello, cano!")
             .padding()
     }
 }
